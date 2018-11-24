d7d4920c268eb17acc1dc7b5b455d553b1c6f6bf
BestCoderz <utk.bkkg@gmail.com>
2018-11-24 12:24:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -173,11 +173,11 @@ async def access(ctx, member: discord.Member):
 @client.command(pass_context = True)
 @commands.has_permissions(administrator=True)
 async def setupwelcomer(ctx):
-	author = ctx.message.author
+    author = ctx.message.author
     server = ctx.message.server
-	everyone_perms = discord.PermissionOverwrite(send_messages=False, read_messages=True)
+    everyone_perms = discord.PermissionOverwrite(send_messages=False, read_messages=True)
     everyone = discord.ChannelPermissions(target=server.default_role, overwrite=everyone_perms)
-	await client.create_channel(server, '★彡-welcome-彡★',everyone)
+    await client.create_channel(server, '★彡-welcome-彡★',everyone)
 
 @client.command(pass_context=True)  
 @commands.has_permissions(kick_members=True)
