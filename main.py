cdb2c24d37117d9be29e00c81cdaee0d5b26e8af
BestCoderz <utk.bkkg@gmail.com>
2018-11-24 16:26:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -124,15 +124,7 @@ async def on_reaction_add(reaction , user):
     embed.add_field(name = 'Setting up Welcomer log(Admin Permission required) ',value ='Use mv!setupwelcomer. It will add a welcome channel. Just put that channel in your desired category and it will send all logs there.',inline = False)
     embed.add_field(name = 'Setting up Giveaway bot(Manage roles permission required) ',value ='Just add a role named ``Giveaways`` and give that role to user who wanna be giveaway manager. Then use ``mv!help`` and check giveaway commands.',inline = False)
     await client.send_message(user,embed=embed)    
-  for channel in user.server.channels:
-    if channel.name == 'i•-general-chat-•i' and reaction.emoji == '😑':
-      r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
-      embed = discord.Embed(title="Successfully added", description="Server Developer role", color = discord.Color((r << 16) + (g << 8) + b))
-      embed.add_field(name="Enjoy! ", value="Happy Server Development. Here you will get special support from our support team related to server development", inline=True)
-      role = discord.utils.get(user.server.roles, name='Server Developer')
-      await client.add_roles(user, role)
-      print('Added codies role in ' + (user.name))
-      await client.send_message(user, embed=embed)
+
 	
 @client.event
 async def on_message(message):
