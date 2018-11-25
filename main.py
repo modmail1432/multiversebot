d16e2bf7d7c75a49b90e954331b91603ed5da2d1
BestCoderz <utk.bkkg@gmail.com>
2018-11-25 19:07:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -145,7 +145,7 @@ async def on_reaction_add(reaction , user):
     embed.add_field(name = 'mv!happy',value ='Happy emoji <a:happy:516183323052212236>',inline = False)
     await client.send_message(user,embed=embed)
   for channel in user.server.channels:
-      if channel.name == '★verify-for-chatting★' and str(reaction.emoji) == '<a:WOW:515854429485006848>':
+      if reaction.message.channel == '★verify-for-chatting★' and reaction.emoji == '🇻':
           role = discord.utils.get(user.server.roles, name='Verified')
           await client.add_roles(user, role)
 
@@ -186,8 +186,8 @@ async def setreactionverify(ctx):
     await client.create_channel(server, '★verify-for-chatting★',everyone)
     for channel in author.server.channels:
         if channel.name == '★verify-for-chatting★':
-            react_message = await client.send_message(channel, 'React with <a:WOW:515854429485006848> to Verify')
-            reaction = ':WOW:515854429485006848'
+            react_message = await client.send_message(channel, 'React with 🇻 to Verify')
+            reaction = '🇻'
             await client.add_reaction(react_message, reaction)
 	
 @client.command(pass_context = True)
