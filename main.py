15a7abb5f67400eb801f7c5b52845fd60887a1b4
BestCoderz <utk.bkkg@gmail.com>
2018-11-26 16:05:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ async def status_task():
         await asyncio.sleep(5)
         await client.change_presence(game=discord.Game(name='with '+str(len(set(client.get_all_members())))+' users'))
         await asyncio.sleep(5)
-        await client.change_presence(game=discord.Game(name='in '+str(len(client.servers))+' servers'))
-        await asyncio.sleep(5)
+         await client.change_presence(game=discord.Game(name='in '+str(len(client.servers))+' servers'))
+         await asyncio.sleep(5)
 	
 @client.event
 async def on_message(message):
