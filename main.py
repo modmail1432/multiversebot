0ac1ba3da1385937d524e6fe847b9866eed01a34
BestCoderz <utk.bkkg@gmail.com>
2018-11-26 12:31:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ from discord import Game, Embed, Color, Status, ChannelType
 import platform
 import os
 import time 
-import json
-import timestamp
+from datetime import datetime
 
 Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
 client = commands.Bot(description="MultiVerse Official Bot", command_prefix="mv!", pm_help = True)
