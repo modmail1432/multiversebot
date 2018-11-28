c96c06e1d129b548bcb0da3bc88b7ad519c1f4f5
BestCoderz <utk.bkkg@gmail.com>
2018-11-28 13:22:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -466,23 +466,20 @@ async def help(ctx):
 @client.command(pass_context=True)  
 @commands.has_permissions(kick_members=True)     
 async def kick(ctx,user:discord.Member):
-
     if user.server_permissions.kick_members:
-        await client.say('**He is mod/admin and i am unable to kick him/her**')
-        return
-    
+      await client.say('**He is mod/admin and i am unable to kick him/her**')
+      return
     else:
-        await client.kick(user)
-	await client.say(user.name+' was kicked. Good bye '+user.name+'!')
-        await client.delete_message(ctx.message)
-	for channel in user.server.channels:
-          if channel.name == '╰☆☆-multiverse-log-☆☆╮':
-              embed=discord.Embed(title="User kicked!", description="**{0}** is kicked by **{1}**!".format(user, ctx.message.author), color=0xFDE112)
-              await client.send_message(channel, embed=embed)
-	
+      await client.kick(user)
+      await client.say(user.name+' was kicked. Good bye '+user.name+'!')
+      await client.delete_message(ctx.message)
+      for channel in user.server.channels:
+        if channel.name == '╰☆☆-multiverse-log-☆☆╮':
+            embed=discord.Embed(title="User kicked!", description="**{0}** is kicked by **{1}**!".format(user, ctx.message.author), color=0xFDE112)
+            await client.send_message(channel, embed=embed)
     except discord.Forbidden:
-        await client.say('Permission denied.')
-        return
+      await client.say('Permission denied.')
+      return
         
 
 @client.command(pass_context = True)
