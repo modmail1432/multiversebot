c8c7bda9ac439b6d2929ba27f45b6f5a8935c6fb
BestCoderz <utk.bkkg@gmail.com>
2018-11-28 13:16:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -473,13 +473,13 @@ async def kick(ctx,user:discord.Member):
     
     try:
         await client.kick(user)
-        await client.say(user.name+' was kicked. Good bye '+user.name+'!')
-        await client.delete_message(ctx.message)
 	for channel in member.server.channels:
           if channel.name == '╰☆☆-multiverse-log-☆☆╮':
               embed=discord.Embed(title="User kicked!", description="**{0}** is kicked by **{1}**!".format(member, ctx.message.author), color=0xFDE112)
               await client.send_message(channel, embed=embed)
-
+              await client.say(user.name+' was kicked. Good bye '+user.name+'!')
+              await client.delete_message(ctx.message)
+	
     except discord.Forbidden:
         await client.say('Permission denied.')
         return
