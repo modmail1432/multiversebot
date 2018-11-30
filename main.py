618dce5e077df258b8b74f3767ba9659857f295e
BestCoderz <utk.bkkg@gmail.com>
2018-11-30 13:15:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -170,6 +170,7 @@ async def on_member_join(member):
             embed.add_field(name='__Thanks for joining__', value='**Hope you will be active here.**', inline=True)
             embed.set_thumbnail(url='https://media.giphy.com/media/OkJat1YNdoD3W/giphy.gif') 
             embed.set_image(url = member.avatar_url)
+            embed.add_field(name='__Join position__', value='{}'.format(str(member.server.member_count)), inline=True)
             embed.add_field(name='Time of joining', value=member.joined_at)
             await client.send_message(channel, embed=embed) 
 
