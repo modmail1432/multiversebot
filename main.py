07d981ba99d56e25c47cda0bb6df3b3a21fd736b
BestCoderz <utk.bkkg@gmail.com>
2018-12-01 12:37:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -211,7 +211,7 @@ async def mute(ctx, member: discord.Member):
 async def avatar(ctx):
     r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
     embed = discord.Embed(title=f'Avatar', description='Avatar is profile picture of a user in discord', color = discord.Color((r << 16) + (g << 8) + b))
-    embed.add_field(name='User: {ctx.message.author.name}', value='Avatar:', inline=True)
+    embed.add_field(name='User: {}'.format(ctx.message.author.name), value='Avatar:', inline=True)
     embed.set_thumbnail(url='https://cdn.discordapp.com/icons/489333893988745217/eb022a7023d013bec656cd7b94d6d6c1.png') 
     embed.set_image(url = ctx.message.author.avatar_url)
     await client.say(embed=embed)
