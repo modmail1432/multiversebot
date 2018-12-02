7a443656d6e2c4ff295a70c93715e75398fa7b92
BestCoderz <utk.bkkg@gmail.com>
2018-12-02 13:15:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -162,10 +162,14 @@ async def on_reaction_add(reaction, user):
         embed.add_field(name = 'mv!alien',value ='Alien emoji <a:alien:517232332663422986>',inline = False)
         embed.add_field(name = 'mv!hi',value ='Saying Hi emoji <a:hi:517232279148429313>',inline = False)
         await client.send_message(user,embed=embed)
-      for channel in user.server.channels:
-        if channel.name == '★verify-for-chatting★' and reaction.emoji == '🇻':
-          role = discord.utils.get(user.server.roles, name='Verified')
-          await client.add_roles(user, role)
+
+    else:
+        for channel in user.server.channels:
+          if channel.name == '★verify-for-chatting★' and reaction.emoji == '🇻':
+              role = discord.utils.get(user.server.roles, name='Verified')
+              await client.add_roles(user, role)
+          else:
+	      return
 	
 @client.event
 async def on_member_join(member):
