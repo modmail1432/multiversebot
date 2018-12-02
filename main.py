d8acb373d3b8a5ca98c17d55dfb47ee34db83009
BestCoderz <utk.bkkg@gmail.com>
2018-12-02 13:28:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import functools
 
 
 Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
-client = commands.Bot(description="MultiVerse Official Bot", command_prefix="mv!", pm_help = True)
+client = commands.Bot(description="MultiVerse Official Bot", command_prefix=commands.when_mentioned_or("mv!"), pm_help = True)
 client.remove_command('help')
 
 async def status_task():
