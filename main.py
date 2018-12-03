1d2b1642e87a09ceed701e4a561a02bddf2f982e
BestCoderz <utk.bkkg@gmail.com>
2018-12-03 17:36:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import platform
 from discord import Game, Embed, Color, Status, ChannelType
 import os
 import functools
-from main.utils.chat_formatting import pagify, box
+from discord.utils.chat_formatting import pagify, box
 
 Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
 client = commands.Bot(description="MultiVerse Official Bot", command_prefix=commands.when_mentioned_or("mv!"), pm_help = True)
