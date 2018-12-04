a1a7a0f1486d83ab526517a78f0625568c15e999
BestCoderz <utk.bkkg@gmail.com>
2018-12-04 11:44:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -335,7 +335,8 @@ async def partner(ctx, *, msg=None):
       await client.say('Please specify a partnership description to post')
     else:
       embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
-      embed.add_field(name='Discord Partner', value='AuthorID: {}'.format(ctx.message.author.id, ctx.message.server.name)) 
+      embed.add_field(name='Discord Partner', value='-------------------') 
+      embed.add_field(name='Partner ID:', value='{}'.format(ctx.message.author.id))
       embed.add_field(name='Partner Name:', value='{}'.format(ctx.message.author.name))
       embed.add_field(name='Server Name:', value='{}'.format(ctx.message.server.name))
       embed.add_field(name='Partnership Description:', value=msg, inline=True)
