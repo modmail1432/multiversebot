7994e404e006880402a74e21ff66de5d4766eb5b
BestCoderz <utk.bkkg@gmail.com>
2018-12-05 20:42:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -335,16 +335,18 @@ async def setuppartner(ctx):
 @commands.cooldown(rate=1,per=86400,type=BucketType.user) 
 @commands.has_permissions(administrator=True)
 async def partner(ctx, *, msg=None):
-    channel = discord.utils.get(client.get_all_channels(), name='★-multiverse-partner-★')
-    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
-    embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
-    embed.add_field(name='Discord Partner', value='-------------------',inline = False) 
-    embed.add_field(name='Partner ID:', value='{}'.format(ctx.message.author.id),inline = False)
-    embed.add_field(name='Partner Name:', value='{}'.format(ctx.message.author.name),inline = False)
-    embed.add_field(name='Server Name:', value='{}'.format(ctx.message.server.name),inline = False)
-    embed.add_field(name='Partnership Description:', value=msg, inline=False)
-    await client.send_message(channel, embed=embed) 
-    await client.delete_message(ctx.message)
+  for server in client.servers:
+    for channel in server.channels:
+      if channel.name == '★-multiverse-partner-★':
+          r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+          embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
+          embed.add_field(name='Discord Partner', value='-------------------',inline = False) 
+          embed.add_field(name='Partner ID:', value='{}'.format(ctx.message.author.id),inline = False)
+          embed.add_field(name='Partner Name:', value='{}'.format(ctx.message.author.name),inline = False)
+          embed.add_field(name='Server Name:', value='{}'.format(ctx.message.server.name),inline = False)
+          embed.add_field(name='Partnership Description:', value=msg, inline=False)
+          await client.send_message(channel, embed=embed) 
+          await client.delete_message(ctx.message)
          
 @client.command(pass_context = True)
 @commands.has_permissions(administrator=True)
