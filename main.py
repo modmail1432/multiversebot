401d1c7c36c1066df615ca76aa7b0fc2c6b6444f
BestCoderz <utk.bkkg@gmail.com>
2018-12-07 11:23:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -234,7 +234,7 @@ async def announce(ctx, channel: discord.Channel=None, *, msg: str):
     r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
     embed=discord.Embed(title="Announcement", description="{}".format(msg), color = discord.Color((r << 16) + (g << 8) + b))
     await client.send_message(channel, embed=embed)
-    
+    await client.delete_message(ctx.message)
 
 @client.command(pass_context = True)
 @commands.has_permissions(kick_members=True) 
