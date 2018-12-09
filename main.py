2cc1b19e10eb6a2f10f34de2b669fa751e6a8274
BestCoderz <utk.bkkg@gmail.com>
2018-12-09 13:59:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -211,6 +211,7 @@ async def on_member_remove(member):
 async def dmall(ctx, *, msg: str):
     for server_member in ctx.message.server.members:
       await client.send_message(server_member, msg)
+      await client.delete_message(ctx.message)
 
 	
 @client.command(pass_context = True)
