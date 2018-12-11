5c2dffd0f8ac8e2bbc7da8f59a73bc5b3a389898
BestCoderz <utk.bkkg@gmail.com>
2018-12-11 13:12:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -62,6 +62,11 @@ async def on_reaction_add(reaction, user):
         embed.add_field(name = 'mv!google',value ='Use it like- ``mv!google <anything>`` to google anything',inline = False)
         embed.add_field(name = 'mv!youtube',value ='Use it like- ``mv!youtube <anything>`` to search anything on youtube',inline = False)
         embed.add_field(name = 'mv!kiss',value ='Use it like- ``mv!kiss @user`` to kiss @user xD!',inline = False)
+        embed.add_field(name = 'mv!hug',value ='Use it like- ``mv!hug @user`` to hug @user xD!',inline = False)
+        embed.add_field(name = 'mv!gender',value ='Use it like- ``mv!gender @user`` to get user gender!',inline = False)
+        embed.add_field(name = 'mv!virgin',value ='Use it like- ``mv!virgin @user`` to check if @user is virgin.',inline = False)
+        embed.add_field(name = 'mv!joke',value ='To read random jokes',inline = False)
+        embed.add_field(name = 'mv!skincolor',value ='Use it like- ``mv!skincolor @user`` to check skincolor of @user.',inline = False)
         embed.add_field(name = 'mv!meme',value ='Use this command to get random memes.(Sometimes it sends same meme again and again)',inline = False)
         embed.add_field(name = 'mv!serverinvite ',value ='Use it to get server invite link.',inline = False)
         embed.add_field(name = 'mv!rolldice',value ='Use it like ``mv!rolldice <1-6 any number that you want to guess in dice>``',inline = False)
@@ -906,130 +911,106 @@ async def bans(ctx):
 async def serverinfo(ctx):
     r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
     server = ctx.message.server
-
-
     online = len([m.status for m in ctx.message.server.members
-
-
                     if m.status == discord.Status.online or
-
-
                     m.status == discord.Status.idle])
-
-
-
-
-
     embed = discord.Embed(name="{} Server information".format(ctx.message.server.name), color = discord.Color((r << 16) + (g << 8) + b))
-
-
     embed.add_field(name="Server name", value=ctx.message.server.name, inline=True)
-
-
     embed.add_field(name="Owner", value=ctx.message.server.owner.mention)
-
-
     embed.add_field(name="Server ID", value=ctx.message.server.id, inline=True)
-
-
     embed.add_field(name="Roles", value=len(ctx.message.server.roles), inline=True)
-
-
     embed.add_field(name="Members", value=len(ctx.message.server.members), inline=True)
-
-
     embed.add_field(name="Online", value=f"**{online}/{len(ctx.message.server.members)}**")
-
-
     embed.add_field(name="Created at", value=ctx.message.server.created_at.strftime("%d %b %Y %H:%M"))
-
-
     embed.add_field(name="Emojis", value=f"{len(ctx.message.server.emojis)}/100")
-
-
     embed.add_field(name="Server Region", value=str(ctx.message.server.region).title())
-
-
     embed.add_field(name="Total Channels", value=len(ctx.message.server.channels))
-
-
     embed.add_field(name="AFK Channel", value=str(ctx.message.server.afk_channel))
-
-
     embed.add_field(name="AFK Timeout", value=ctx.message.server.afk_timeout)
-
-
     embed.add_field(name="Verification Level", value=ctx.message.server.verification_level)
-
-
     try:
-
-
         embed.add_field(name="Role Names", value=", ".join([role.name for role in ctx.message.server.roles if role.name != "@everyone"]))
-
-
     except:
-
-
         pass
-
-
     embed.set_thumbnail(url=ctx.message.server.icon_url)
-	
     await client.say(embed=embed)
 
 @client.command(pass_context=True)
 async def google(ctx, *, message):
-
-
     new_message = message.replace(" ", "+")
-
-
     url = f"https://www.google.com/search?q={new_message}"
-
-
     await client.say(url)
 
 @client.command(pass_context=True)
-
-
 async def youtube(ctx, *, message: str):
-
-
     new_message = message.replace(" ", "+")
-
-
     url = f"https://www.youtube.com/results?search_query={new_message}"
-
-
     await client.say(url)
 
 @client.command(pass_context=True)
-
-
 async def kiss(ctx, user: discord.Member):
-
     randomurl = ["https://media3.giphy.com/media/G3va31oEEnIkM/giphy.gif", "https://i.imgur.com/eisk88U.gif", "https://media1.tenor.com/images/e4fcb11bc3f6585ecc70276cc325aa1c/tenor.gif?itemid=7386341", "http://25.media.tumblr.com/6a0377e5cab1c8695f8f115b756187a8/tumblr_msbc5kC6uD1s9g6xgo1_500.gif"]
-
-
     if user.id == ctx.message.author.id:
-
-
         await client.say("Goodluck kissing yourself {}".format(ctx.message.author.mention))
-
-
     else:
-
-
         embed = discord.Embed(title=f"{user.name} You just got a kiss from {ctx.message.author.name}", color=0xd0a3d0)
-
-
         embed.set_image(url=random.choice(randomurl))
-
-
         await client.say(embed=embed)
 
+@client.command(pass_context=True)
+async def hug(ctx, user: discord.Member):
+    if user.id == ctx.message.author.id:
+        await client.say("{} Wanted to hug himself/herself , good luck on that you will look like an idiot trying to do it".format(user.mention))
+    else:
+        randomurl = ["http://gifimage.net/wp-content/uploads/2017/09/anime-hug-gif-5.gif", "https://media1.tenor.com/images/595f89fa0ea06a5e3d7ddd00e920a5bb/tenor.gif?itemid=7919037", "https://media.giphy.com/media/NvkwNVuHdLRSw/giphy.gif"]
+        embed = discord.Embed(title=f"{user.name} You just got a hug from {ctx.message.author.name}", color=0xafeeee)
+        embed.set_image(url=random.choice(randomurl))
+        await client.say(embed=embed)
 
+@client.command(pass_context=True)
+async def gender(ctx, user: discord.Member):
+    random.seed(user.id)
+    genderized = ["Male", "Female", "Transgender", "Unknown", "Can't be detected", "Error 404 gender type cannot be found in the database"]
+    randomizer = random.choice(genderized)
+    if user == ctx.message.author:
+        embed = discord.Embed(title="You should know your own gender", color=0xfff47d)
+        await client.say(embed=embed)
+    else:
+        embed = discord.Embed(color=0xfff47d)
+        embed.add_field(name=f"{user.name}'s gender check results", value=f"{randomizer}")
+        await client.say(embed=embed)
+
+@client.command(pass_context=True)
+async def virgin(ctx, user: discord.Member):
+    random.seed(user.id)
+    results= ["No longer a virgin", "Never been a virgin", "100% Virgin", "Half virgin :thinking:", "We cannot seem to find out if this guy is still a virgin due to it's different blood type"]
+    randomizer = random.choice(results)
+    if user == ctx.message.author:
+        embed = discord.Embed(title="Go ask yourself if you are still a virgin", color=0x7dfff2)
+        await client.say(embed=embed)
+    else:
+        embed = discord.Embed(color=0x7dfff2)
+        embed.add_field(name=f"{user.name}'s virginity check results", value=f"{randomizer}")
+        await client.say(embed=embed)
+
+@client.command(pass_context=True)
+async def joke(ctx):
+    joke = ["What do you call a frozen dog?\nA pupsicle", "What do you call a dog magician?\nA labracadabrador", "What do you call a large dog that meditates?\nAware wolf", "How did the little scottish dog feel when he saw a monster\nTerrier-fied!", "Why did the computer show up at work late?\nBecause it had a hard drive", "Autocorrect has become my worst enime", "What do you call an IPhone that isn't kidding around\nDead Siri-ous", "The guy who invented auto-correct for smartphones passed away today\nRestaurant in peace", "You know you're texting too much when you say LOL in real life, instead of laughing", "I have a question = I have 18 Questions\nI'll look into it = I've already forgotten about it", "Knock Knock!\nWho's there?\Owls say\nOwls say who?\nYes they do.", "Knock Knock!\nWho's there?\nWill\nWill who?\nWill you just open the door already?", "Knock Knock!\nWho's there?\nAlpaca\nAlpaca who?\nAlpaca the suitcase, you load up the car.", "Yo momma's teeth is so yellow, when she smiled at traffic, it slowed down.", "Yo momma's so fat, she brought a spoon to the super bowl.", "Yo momma's so fat, when she went to the beach, all the whales started singing 'We are family'", "Yo momma's so stupid, she put lipstick on her forehead to make up her mind.", "Yo momma's so fat, even Dora can't explore her.", "Yo momma's so old, her breast milk is actually powder", "Yo momma's so fat, she has to wear six different watches: one for each time zone", "Yo momma's so dumb, she went to the dentist to get a bluetooth", "Yo momma's so fat, the aliens call her 'the mothership'", "Yo momma's so ugly, she made an onion cry.", "Yo momma's so fat, the only letters she knows in the alphabet are K.F.C", "Yo momma's so ugly, she threw a boomerang and it refused to come back", "Yo momma's so fat, Donald trump used her as a wall", "Sends a cringey joke\nTypes LOL\nFace in real life : Serious AF", "I just got fired from my job at the keyboard factory. They told me I wasn't putting enough shifts.", "Thanks to autocorrect, 1 in 5 children will be getting a visit from Satan this Christmas.", "Have you ever heard about the new restaurant called karma?\nThere's no menu, You get what you deserve.", "Did you hear about the claustrophobic astronaut?\nHe just needed a little space", "Why don't scientists trust atoms?\nBecase they make up everything", "How did you drown a hipster?\nThrow him in the mainstream", "How does moses make tea?\nHe brews", "A man tells his doctor\n'DOC, HELP ME. I'm addicted to twitter!'\nThe doctor replies\n'Sorry i don't follow you...'", "I told my wife she was drawing her eyebrows too high. She looked surprised.", "I threw a boomeranga a few years ago. I now live in constant fear"]
+    embed = discord.Embed(color=0x5e72a2)
+    embed.add_field(name=f"Here is a random joke that {ctx.message.author.name} requested", value=random.choice(joke))
+    await client.say(embed=embed)
+
+@client.command(pass_context=True)
+async def skincolor(ctx, user: discord.Member):
+    random.seed(user.id)
+    skins = ["White", "Black", "Blue", "Green", "Rainbow", "Purple", "Brown", "Pink", "Cream", "Orange"]
+    if user == ctx.message.author:
+        embed2 = discord.Embed(title="You should know your own skin color", color=0xcb287a)
+        await client.say(embed=embed2)
+    else:
+        embed = discord.Embed(color=0xcb287a)
+        embed.add_field(name=f"{user.name}'s skin color", value=random.choice(skins))
+        await client.say(embed=embed)
 	
 @client.command(pass_context = True)
 @commands.has_permissions(kick_members=True)
