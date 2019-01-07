6ceba506b90a1b77177ce3277c238a8d5419fdbf
BestCoderz <utk.bkkg@gmail.com>
2019-01-07 12:14:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ import json
 import praw
 import aiohttp
 from imgurpython import ImgurClient
+from random import choice, shuffle
 
 Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
 client = commands.Bot(description="MultiVerse Official Bot", command_prefix=commands.when_mentioned_or("mv!" ), pm_help = True)
